a2b551f09e7224e079f5284a5bd7aa64dd9ae276
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-06-02 15:37:09 +0300

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -25,6 +25,53 @@ def afk(bot: Bot, update: Update):
     update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
 
 
+@run_async
+def no_longer_afk(bot: Bot, update: Update):
+    user = update.effective_user  # type: Optional[User]
+
+    if not user:  # ignore channels
+        return
+
+    res = sql.rm_afk(user.id)
+    if res:
+        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+
+
+@run_async
+def reply_afk(bot: Bot, update: Update):
+    message = update.effective_message  # type: Optional[Message]
+    entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+    if message.entities and entities:
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                user_id = ent.user.id
+                fst_name = ent.user.first_name
+
+            elif ent.type == MessageEntity.MENTION:
+                user_id = get_user_id(message.text[ent.offset:ent.offset + ent.length])
+                if not user_id:
+                    # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
+                    return
+                chat = bot.get_chat(user_id)
+                fst_name = chat.first_name
+
+            else:
+                return
+
+            if sql.is_afk(user_id):
+                valid, reason = sql.check_afk_status(user_id)
+                if valid:
+                    if not reason:
+                        res = "{} is AFK!".format(fst_name)
+                    else:
+                        res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+                    message.reply_text(res)
+
+
+def __gdpr__(user_id):
+    sql.rm_afk(user_id)
+
+
 __help__ = """
  - /afk <reason>: mark yourself as AFK.
  - brb <reason>: same as the afk command - but not a command.
